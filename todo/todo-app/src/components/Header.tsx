daadefdeeab7fed0c8ac27f793d767be8d0c4c36
yaw.o.k <obengky72@gmail.com>
2024-10-10 13:14:46 +0200

    Add header

---
+++ todo/todo-app/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+function Header() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+				<Container>
+					<Navbar.Brand href="/" >ToDo</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="/new">Add</Nav.Link>
+						<Nav.Link href="/all">View All</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+    </>
+  );
+}
+
+export default Header;
