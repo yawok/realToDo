f395b26c730b631fb6381e01aca7979586798661
yaw.o.k <obengky72@gmail.com>
2024-10-23 18:31:34 +0200

    Remove useless white space

--- todo/todo-app/src/services/firebase.ts
+++ todo/todo-app/src/services/firebase.ts
@@ -1,20 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { config } from "dotenv";
-
-config();
 const env = process.env;
 
 const firebaseConfig = {
-  apiKey: env.REACT_APP_API_KEY,
-  authDomain: env.REACT_APP,
-  projectId: env.PROJECT_ID,
-  storageBucket: env.STORAGE_BUCKET,
-  messagingSenderId: env.MESSSAGING_SENDER_ID,
-  appId: env.APP_ID,
-  measurementId: env.MEASUREMENT_ID,
+  apiKey: env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.REACT_APP_FIREBASE_MESSSAGING_SENDER_ID,
+  appId: env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+console.log(typeof(firebaseConfig.databaseURL))
+
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
