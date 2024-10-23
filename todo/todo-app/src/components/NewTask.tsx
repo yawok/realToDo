9d63df45bbe458611dedd04ff87a20c1ef9a4073
yaw.o.k <obengky72@gmail.com>
2024-10-23 18:39:49 +0200

    Add values to form fields

--- todo/todo-app/src/components/NewTask.tsx
+++ todo/todo-app/src/components/NewTask.tsx
@@ -1,37 +1,47 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button, Form, Container } from "react-bootstrap";
+import { saveTask } from "../services/dbService";
 
 export interface Task {
+  taskId?: string;
   title: string;
   description: string;
   isDone: boolean;
-  startDate: Date;
-  deadline: Date | null;
+  startDate: number;
+  deadline: number;
 }
 
 function NewTask() {
+  const today = new Date().toISOString();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [startDate, setStartDate] = useState("2024-10-10");
+  const [startDate, setStartDate] = useState(today.slice(0, 10));
   const [deadline, setDeadline] = useState("");
 
-  const saveTask = () : void => {
+  const saveNewTask = () : void => {
     const newTask : Task = {
       title : title,
       description : description,
       isDone : false,
-      startDate : new Date(startDate),
-      deadline : new Date (deadline)
+      startDate : new Date(startDate).getTime(),
+      deadline : new Date (deadline).getTime()
     };
 
-    // TODO: write code to save
-    console.log(newTask);
+    saveTask(newTask).then(
+      () => {
+        setTitle("");
+        setDescription("");
+        setStartDate(today.slice(0, 10));
+        setDeadline("");
+      }
+    );
   };
 
   const setValue = (e: any) => {
     const element: any = e.target;
     switch (element.name) {
       case "title":
+        console.log(title);
         setTitle(element.value);
         break;
       case "description":
@@ -55,22 +65,22 @@ function NewTask() {
         <Form>
           <Form.Group className="mb-2">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="Enter a title" name="title" onChange={setValue}/>
+            <Form.Control type="text" placeholder="Enter a title" name="title" value={title} onChange={setValue}/>
           </Form.Group>
           <Form.Group className="mb-2">
             <Form.Label>Description</Form.Label>
-            <Form.Control type="text" placeholder="Enter a description" name="description" onChange={setValue}/>
+            <Form.Control type="text" placeholder="Enter a description" name="description" value={description} onChange={setValue}/>
           </Form.Group>
           <Form.Group className="mb-2">
             <Form.Label>Start Date</Form.Label>
-            <Form.Control type="date" name="startDate" onChange={setValue}/>
+            <Form.Control type="date" name="startDate" value={startDate} onChange={setValue}/>
           </Form.Group>
           <Form.Group className="mb-2">
             <Form.Label>Deadline</Form.Label>
-            <Form.Control type="date" name="deadline" onChange={setValue}/>
+            <Form.Control type="date" name="deadline" value={deadline} onChange={setValue}/>
           </Form.Group>
         </Form>
-        <Button variant="primary" type="submit" onClick={saveTask}>
+        <Button variant="primary" type="submit" onClick={saveNewTask}>
           Save
         </Button>
       </Container>
